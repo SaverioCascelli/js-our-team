c1947d6162c6087cc2a4fbcdaa169b23b56536a5
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-25 15:47:14 +0200

    [add] cardRow in js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+
+const cardRow = document.createElement("div");
+cardRow.classList.add("row","h-100","align.content-start");
+
+container.append(cardRow);
