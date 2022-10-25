0f68f475b0da86681bf6dfd671c4c8134a883fe4
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-25 16:07:25 +0200

    [add] console collection display **Milestone 1**

--- js/script.js
+++ js/script.js
@@ -4,3 +4,46 @@ const cardRow = document.createElement("div");
 cardRow.classList.add("row","h-100","align.content-start");
 
 container.append(cardRow);
+
+const staff =[
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+for (const member of staff) {
+    let st = "";
+    st += 
+`<-------------------------------------------------->
+nome: ${member.name}
+ruolo: ${member.role}
+image src = ${member.image}`;
+    console.log(st);
+}
