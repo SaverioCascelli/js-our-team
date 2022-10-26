f3c6397a1703f0f9dc80c14e60ebf08aedc4110f
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-26 07:56:58 +0200

    [add] qualities pops up in hover in card

--- js/script.js
+++ js/script.js
@@ -11,34 +11,34 @@ container.append(cardRow);
 //Creare l’array di oggetti con le informazioni fornite.
 const staff =[
     {
-        name: "Wayne Barnett",
+        name:"Wayne Barnett",
         role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        image: "wayne-barnett-founder-ceo.jpg",
     },
     {
-        name: "Angela Caroll",
+        name:"Angela Caroll",
         role: "Chief Editor",
-        image: "angela-caroll-chief-editor.jpg"
+        image: "angela-caroll-chief-editor.jpg",
     },
     {
         name: "Walter Gordon",
         role: "Office Manager",
-        image: "walter-gordon-office-manager.jpg"
+        image: "walter-gordon-office-manager.jpg",
     },
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
-        image: "angela-lopez-social-media-manager.jpg"
+        image: "angela-lopez-social-media-manager.jpg",
     },
     {
         name: "Scott Estrada",
         role: "Developer",
-        image: "scott-estrada-developer.jpg"
+        image: "scott-estrada-developer.jpg",
     },
     {
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        image: "barbara-ramos-graphic-designer.jpg"
+        image: "barbara-ramos-graphic-designer.jpg",
     }
 ];
 
@@ -46,7 +46,14 @@ printInfo();
 
 staffInCard();
 
-// -----------------**Milestone 1**----------------------------------------
+addQualities();
+addQualityInHtml();
+console.log(staff);
+
+
+
+
+//-----------------**Milestone 1**----------------------------------------
 //Stampare su console le informazioni di nome, ruolo e la stringa della foto
 for (const member of staff) {
     let st = "";
@@ -79,19 +86,59 @@ function printInfo(){
 }
 
 
-// -----------------**Milestone 3**----------------------------------------
+// -----------------**Milestone 3 + Bonus**----------------------------------------
 //Stampare delle card formattate contenete immagini e testo
+//Aggiungere degli aggettivi (più di uno) che identifichino ogni persona e stamparli nella card
 function staffInCard(){
     for (const member of staff) {
-        cardRow.innerHTML += `<div class="col-4 mb-5">
-                        <div class="card text-center" style="width: 18rem;">
-                            <img src="img/${member.image}" class="card-img-top" alt="${member.name}">
-                            <div class="card-body">
-                                <h5 class="text-secondary">${member.name}</h5>
-                                <p class="text-secondary card-text">${member.role}</p>           
+
+
+         cardRow.innerHTML += `<div class="col-4 mb-5">
+                            <div class="card text-center" style="width: 18rem" name="${member.name}">
+                                <div class="card-img-top position-relative overflow-hidden">
+                                    <img class="card-img-top" src="img/${member.image}"  alt="${member.name}">
+                                    <div class="sc-toggle" >
+                                    </div>    
+                                </div>
+                                
+                                <div class="card-body">
+                                    <h5 class="text-secondary">${member.name}</h5>
+                                    <p class="text-secondary card-text">${member.role}</p>          
+                                </div>
                             </div>
-                        </div>
-                    </div>`
+                        </div>`
         
     }
 }
+
+
+// -----------------**Bonus**----------------------------------------
+
+function addQualities(){
+    staff[0].qualities = ["Intuitive","Passionate","Leadership"];
+    staff[1].qualities = ["Intuitive","Passionate","Friendly"];
+    staff[2].qualities = ["Intuitive","Passionate","Leadership"];
+    staff[5].qualities = ["Intuitive","Passionate","Leadership"];
+}
+
+function addQualityInHtml(){
+    const htmlQualities = document.getElementsByClassName("sc-toggle");
+    for (const memberIndex in staff) {
+        if(staff[memberIndex].qualities){
+            let height = 15;
+            for (const quality of staff[memberIndex].qualities) {
+                htmlQualities[memberIndex].innerHTML += `<span class="position-absolute badge rounded-pill text-bg-primary " style="bottom:${height}%;">${quality}</span>`;
+                height += 10;
+            }
+           height =15; 
+        }
+    }
+}
+
+
+
+
+
+
+
+
